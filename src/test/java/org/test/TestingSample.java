0afb82065710bf6d555c5a842b52f3937c4706ae
DhivyaBharathi-tester <dhivyalasha07@gmail.com>
2022-11-09 14:38:18 +0530

    project demo

---
+++ src/test/java/org/test/TestingSample.java
@@ -0,0 +1,31 @@
+package org.test;
+
+import org.junit.Test;
+
+public class TestingSample {
+
+	
+		@Test
+	private void test() {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
